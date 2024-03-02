ba6bf1cd4b42815607fb08403b7e67e693c15bd4
PrCHouds <bystwkir@gmail.com>
2024-03-02 20:37:42 +0700

    Init Model

--- model.py
+++ model.py
@@ -2,6 +2,16 @@ from tensorflow import keras
 from tensorflow.keras.layers import Input, Dense, LSTM
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.utils import to_categorical
-import numpy
-def init_Model():
+
+def init_Model(depth, categories):
     model = Sequential()
+    model.add(Input(shape=(depth, categories)))
+    model.add(LSTM(units= 20, activation= 'sigmoid'))
+    model.add(Dense(categories, activation='sigmoid'))
+    model.compile(loss= 'categorical_crossentropy', metrics= ['accuracy', 'mse'])
+    return model
+
+# def trainingAndSave(model, x_train, y_train):
+
+
+
