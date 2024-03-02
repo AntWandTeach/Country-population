5847aa6d4bae40f89d92f91ed73bccef4ccc4296
neparovoz <zaharovdenis03@gmail.com>
2024-03-02 20:16:15 +0700

    Init_commit

---
+++ Config.py
@@ -0,0 +1,9 @@
+import os
+
+num_of_categories = 100
+depth_ = 5
+begin_year = 1960
+end_year = 2022
+
+directories = {'datasets': 'datasets/', 'models': 'models/', 'results':'results/', 'tables': 'tables/' }
+
---
+++ main.py
@@ -0,0 +1,4 @@
+import Config, random
+
+if __name__ == '__main__':
+    print()
