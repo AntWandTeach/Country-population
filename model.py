9fcd31bae27b72fb6442993d9dec6351f10843f1
PrCHouds <bystwkir@gmail.com>
2024-03-07 17:01:28 +0700

    Dropout to Function

--- model.py
+++ model.py
@@ -1,17 +1,20 @@
 from tensorflow import keras
-from tensorflow.keras.layers import Input, Dense, LSTM
+from tensorflow.keras.layers import Input, Dense, LSTM, Dropout
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.utils import to_categorical
 
 def init_Model(depth, categories):
     model = Sequential()
     model.add(Input(shape=(depth, categories)))
+    model.add(Dropout(0.2))
     model.add(LSTM(units= 20, activation= 'sigmoid'))
+    model.add(Dropout(0.2))
     model.add(Dense(categories, activation='sigmoid'))
     model.compile(loss= 'categorical_crossentropy', metrics= ['accuracy', 'mse'])
     return model
 
-# def trainingAndSave(model, x_train, y_train):
-
+def trainingAndSave(model, x_train, y_train, model_num):
+    model.fit(x_train, y_train, epochs= 10)
+    model.save('models')
 
 
