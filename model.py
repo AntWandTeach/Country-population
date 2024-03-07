f248c140f666c544072f3016c3daaaad7829b7e1
PrCHouds <bystwkir@gmail.com>
2024-03-07 17:23:45 +0700

    New Model params and Train - Save function

--- model.py
+++ model.py
@@ -2,19 +2,21 @@ from tensorflow import keras
 from tensorflow.keras.layers import Input, Dense, LSTM, Dropout
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.utils import to_categorical
+from Config import *
 
 def init_Model(depth, categories):
     model = Sequential()
     model.add(Input(shape=(depth, categories)))
-    model.add(Dropout(0.2))
-    model.add(LSTM(units= 20, activation= 'sigmoid'))
-    model.add(Dropout(0.2))
+    model.add(Dropout(dropout_ratio))
+    model.add(LSTM(units= input_size_lstm, activation= 'sigmoid'))
+    model.add(Dropout(dropout_ratio))
     model.add(Dense(categories, activation='sigmoid'))
     model.compile(loss= 'categorical_crossentropy', metrics= ['accuracy', 'mse'])
     return model
 
-# def trainingAndSave(model, x_train, y_train, model_num):
-#     model.fit(x_train, y_train, epochs= 10)
-#     model.save('models')
+def trainingAndSave(model, x_train, y_train, model_name = '0'):
+    model.fit(x_train, y_train, epochs= nums_epochs, validation_split = valid)
+    filename = 'models/model_' + model_name
+    model.save(filename)
 
 
